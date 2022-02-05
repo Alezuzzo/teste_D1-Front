2e25c7bffadbe11d17c80dbbf6406e41491ea971
Thalles Alexsander <thallesander@hotmail.com>
2022-02-05 11:12:33 -0300

    iniciando modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import React, { useState } from 'react'
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Modal from './components/Modal'
 import Table from './components/Table'
+import { BiLeftArrow } from 'react-icons/bi';
+
+const ButtonWrapperStyle = {
+  position: 'relative',
+  zindex: 1,
+}
+
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="App">
-        <Sidebar/>
-        <Header/>
-        
-    </div>
-  );
+    <>
+      <div className="App">
+            <Sidebar/>
+            <Header/>
+
+            <div style={ButtonWrapperStyle} onClick={() => console.log('clicked')}>
+              <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+              <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+                Fancy Modal
+              </Modal>
+            </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import reactDom from 'react-dom';
 
-function Modal() {
-  return (
-    <div className='Modal'>
-        <div className="ModalContent">
-            <p className="TextJourney">Nova Jornada</p>
-            <p>Dê um nome para essa Jornada</p>
-            <input></input>
-            <p>Você poderá alterar essa informação depois.</p>
-            <button>Continuar</button>
-            <button>Cancelar</button>
-        </div>    
-    </div>
-  )
+const ModalStyles = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFF',
+  padding: '50px',
+  zIndex: 1000
 }
 
-export default Modal;
+const OverlayStyle = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0,0,0, .7)',
+  zIndex: 1000
+}
+
+export default function Modal({ open, children, onClose }) {
+  if (!open) return null
+
+  return reactDom.createPortal(
+    <>
+      <div style = {OverlayStyle} />
+      <div style={ModalStyles} >
+        <button onClick={onClose}>Close Modal</button>
+        {children}        
+      </div>
+    
+    </>,
+    document.getElementById('portal')
+  )
+};
